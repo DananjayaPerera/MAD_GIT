f92910fbd3255c41cc3986fb3738f397e14c7021
DananjayaPerera <53034152+DananjayaPerera@users.noreply.github.com>
2019-07-18 12:55:32 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Sytem.out.println ("Hello");
+        System.out.println ("Hello");
     }
 }
